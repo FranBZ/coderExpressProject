43ee2aeaddaa54d55d9cc9f1e918449ccdcfaad4
FranBZ <franciscobenitezjan@gmail.com>
2022-08-02 19:57:13 -0300

    actualizacion clase

--- src/class.js
+++ src/class.js
@@ -1,4 +1,4 @@
-/* >> Consigna: Implementar programa que contenga una clase llamada Contenedor que reciba el
+/*    Consigna: Implementar programa que contenga una clase llamada Contenedor que reciba el
       nombre del archivo con el que va a trabajar e implemente los siguientes métodos:
         ● save(Object): Number - Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
         ● getById(Number): Object - Recibe un id y devuelve el objeto con ese id, o null si no está.
@@ -10,16 +10,16 @@ const fs = require('fs')
 
 class Contenedor {
 
-    constructor (fileName) {        // Constuctor, recibe como parametro el nombre del archivo
-        this.fileName = fileName    // Nombre del archivo
+    constructor (fileName) {  // Constuctor, recibe como parametro el nombre del archivo
+        this.fileName = fileName  // Nombre del archivo
     }
 
     async readAndParseFile (file) {  // Esta funcion se utiliza para leer el archivo y parsear a JSON la informacion, para su posterior uso
 
         try {
             const data = await fs.promises.readFile(file, 'utf-8', (err, data) => {         // Consultamos por la informacion
-            if(err) throw err
-            return data
+                if(err) throw err
+                return data
             })
             return JSON.parse(data)                                                         // Retornamos la informacion parseada
         } catch (error) {
@@ -30,19 +30,21 @@ class Contenedor {
     async save (product) {        // Guarda un archivo nuevo en el array de productos
 
         try {
-            if (fs.existsSync(this.fileName)){                                              // Si existe
-                const data = await this.readAndParseFile(this.fileName)                     // Nos traemos la info parseada a JSON del archivo leido
-                const idProduct = data[data.length -1].id + 1                               // Asignamos el ID siguiente al ultimo asignado
-                product.id = idProduct                                                      // Insertamos el ID en el producto
-                data.push(product)                                                          // Pusheamos el producto en el array
-                await fs.writeFile(this.fileName, JSON.stringify(data, null, 2), err => {   // Creamos el archivo y lo escribimos con los objetos del array
+            if (fs.existsSync(this.fileName)){                                                      // Si existe
+                const data = await this.readAndParseFile(this.fileName)                             // Nos traemos la info parseada a JSON del archivo leido
+                const idProduct = data[data.length -1].id + 1                                       // Asignamos el ID siguiente al ultimo asignado
+                product.id = idProduct                                                              // Insertamos el ID en el producto
+                data.push(product)                                                                  // Pusheamos el producto en el array
+                await fs.promises.writeFile(this.fileName, JSON.stringify(data, null, 2), err => {  // Creamos el archivo y lo escribimos con los objetos del array
                     if(err) throw err
                 })
-            } else {                                                                        // Si no existe
-                product.id = 1                                                              // Asignamos ID 1 al rpoducto
-                const data = [product]                                                      // Pusheamos el producto en el array
-                await fs.writeFile(this.fileName, JSON.stringify(data, null, 2), err => {   // Creamos el archivo y lo escribimos con los objetos del array
+                console.log(`El ID asignado a ${product.title} es: ${product.id}`)
+            } else {                                                                                // Si no existe
+                product.id = 1                                                                      // Asignamos ID 1 al rpoducto
+                const data = [product]                                                              // Pusheamos el producto en el array
+                await fs.promises.writeFile(this.fileName, JSON.stringify(data, null, 2), err => {  // Creamos el archivo y lo escribimos con los objetos del array
                     if(err) throw err
+                    console.log(`El ID asignado a ${product.title} es: 1`)
                 })
             }     
         } catch (error) {
@@ -50,12 +52,12 @@ class Contenedor {
         }
     }
 
-    async getById (id) {    // Esta funcion devuelve un producto segun su ID
+    async getById (id) {  // Esta funcion devuelve un producto segun su ID
 
         try {
-            const data = await this.readAndParseFile(this.fileName)                         // Nos traemos la info parseada a JSON del archivo leido
-            const object = data.filter(product => product.id == id)                         // Filtramos por ID y lo retornamos
-            if (object.length !== 0) { return object } else { return null }                 // Comprobamos si el array esta vacio o hay algun elemento e imprimimos por consola
+            const data = await this.readAndParseFile(this.fileName)             // Nos traemos la info parseada a JSON del archivo leido
+            const object = data.filter(product => product.id == id)             // Filtramos por ID y lo retornamos
+            if (object.length !== 0) { return object } else { return null }     // Comprobamos si el array esta vacio o hay algun elemento e imprimimos por consola
         
         } catch (error) {
             console.error(`El error es: ${error}`)
@@ -64,20 +66,20 @@ class Contenedor {
 
     async getAll () {  // Esta funcion devuelve todos los productos del archivo
         try {
-            const data = await this.readAndParseFile(this.fileName)                         // Nos traemos la info parseada a JSON del archivo leido e imprimimos por consola
+            const data = await this.readAndParseFile(this.fileName) // Nos traemos la info parseada a JSON del archivo leido e imprimimos por consola
             return data
         } catch (error) {
             console.error(`El error es: ${error}`)
         }
     }
 
-    async deleteById (id) { // Esta funcion devuelve un producto segun su ID
+    async deleteById (id) { // Esta funcion elimina un producto segun su ID
         try {
             const data = await this.readAndParseFile(this.fileName)
             let newData = data.filter(product => product.id != id)
 
-            await fs.writeFile(this.fileName, JSON.stringify(newData, null, 2), err => {
-            if(err) throw err
+            await fs.promises.writeFile(this.fileName, JSON.stringify(newData, null, 2), err => {
+                if(err) throw err
             })
 
         } catch (error) {
@@ -85,10 +87,10 @@ class Contenedor {
         }
     }
 
-    async delteAll () {     //Esta funcion elimina todos los productos
+    async deleteAll () { //Esta funcion elimina todos los productos
         try {
-            await fs.writeFile(this.fileName, '[]', err => {         // No eliminamos los productos, sino que reescribimos el archivo con un array vacio
-            if(err) throw err
+            await fs.promises.writeFile(this.fileName, '[]', err => { // No eliminamos los productos, sino que reescribimos el archivo con un array vacio
+                if(err) throw err
             })
         } catch (error) {
             console.error(`El error es: ${error}`)
